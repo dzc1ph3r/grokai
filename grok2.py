4a14a00e8d9125b38e39f2f4c79f3681adfa7073
Nacereddine GACEB <135277783+dzc1ph3r@users.noreply.github.com>
2024-11-05 17:19:22 +0100

    Add files via upload

---
+++ grok2.py
@@ -0,0 +1,32 @@
+import requests
+
+
+user_content = input("Enter your prompt: ")
+
+url = "https://api.x.ai/v1/chat/completions"
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": ""  # Your API from https://console.x.ai/
+}
+data = {
+    "messages": [
+        {
+            "role": "system",
+            "content": "You are a Grok."
+        },
+        {
+            "role": "user",
+            "content": user_content  
+        }
+    ],
+    "model": "grok-beta",
+    "stream": False,
+    "temperature": 0
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+
+content = response.json().get("choices", [{}])[0].get("message", {}).get("content")
+print(content)
+
